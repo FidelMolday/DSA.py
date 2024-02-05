c97d5e73df47bea33c54084b3f82cc93975a8e19
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-02-05 04:45:02 +0300

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,8 @@
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+print(browsing_session)
+last = browsing_session.pop()
+print(last)
+print(browsing_session)
